96916aa183d2d3b02488e2dd1a75b4181274ac2a
Sunny-Dee <delianaescobari@gmail.com>
2014-04-25 14:29:39 -0500

    one player can fill up the board

--- lib/ticgame.rb
+++ lib/ticgame.rb
@@ -1,16 +1,11 @@
 class Game
 
-# Global variables
-turn = 0
-@board=["0","1","2","3","4","5","6","7","8"]
-winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
-
 # Helper functions
 	def new_game
 		puts "Basic Tic Tac Toe"
-		@board =["0","1","2","3","4","5","6","7","8"]
-		winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+		@board =["0","1","2","3","4","5","6","7","8"] 
+		@winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+		@turn = 1
 		draw_board
 		play
 	end
@@ -29,34 +24,19 @@ winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],
 # Main logic functions 
 
 	def play
-		draw_board
-		puts "Choose the number of the cell you want to mark"
-		choice = gets.to_i
-		if turn % 2 != 0
-			winning_combinations.gsub!(choice, 'x')
-			@board.insert(choice, 'x')
-		elsif turn % 2 == 0
-			winning_combinations.gsub(choice, 'o')
-			@board.insert(choice, 'o')
-		end
-		draw_board
-		check_for_winners
-		turn += 1
-	end
-
-
-	def check_for_winners
-		winning_combinations.each |combo|
-			if combo == ['x', 'x', 'x'] 
-				puts 'x wins'
-			elsif combo == ['o', 'o', 'o']
-				puts 'o wins'
-			end	
-	end
-
+		while @turn <= 9
+			puts "Choose the number of the cell you want to mark"
+			choice = gets.to_i
+	 		#if turn % 2 != 0
+	 		@board[choice] = 'x'
+	 		print draw_board
+	 		@turn += 1
+	 		check_for_draws
+	 	end
+  	end	
 
 	def check_for_draws
-		if turn > 9 
+		if @turn > 9 
 			puts "Game Over"
 			new_game
 		else
@@ -65,3 +45,5 @@ winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],
 	end
 
 end
+
+Game.new.new_game

29c0f941fde762607f2d02bd07dda16cc075a455
Sunny-Dee <delianaescobari@gmail.com>
2014-04-25 08:50:59 -0500

    Update ticgame.rb

--- ticgame.rb
+++ ticgame.rb
@@ -17,6 +17,7 @@ winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],
 		#winning_combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 		draw_board
 		play
+		turn = 0
 	end
 
 	def draw_board

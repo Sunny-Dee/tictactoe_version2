14bd69efdc50c2f17da2f99b8bab78b452a38bde
Sunny-Dee <delianaescobari@gmail.com>
2014-04-26 23:16:17 -0500

    game asks player if they want to start a new game

--- lib/ticgame.rb
+++ lib/ticgame.rb
@@ -1,6 +1,20 @@
 class Game
 
 # Helper functions
+	def greet
+		puts '     '
+		puts 'Would you like to play tic tac toe?'
+		puts '     -This game requires two players'
+		answer = gets.chomp.downcase
+			if answer == 'yes'
+				new_game
+			else
+				puts 'Ok. Bye then.'
+				puts '  '
+				greet
+			end
+	end
+
 	def new_game
 		puts " "
 		puts "Basic Tic Tac Toe"
@@ -13,12 +27,15 @@ class Game
 
 	def draw_board
 		puts " "
+		puts " "
 		puts " #{@board[0]}   |  #{@board[1]}     |  #{@board[2]}  "
 		puts "_____|________|_____"
 		puts " #{@board[3]}   |  #{@board[4]}     |  #{@board[5]}  "
 		puts "_____|________|_____"
 		puts " #{@board[6]}   |  #{@board[7]}     |  #{@board[8]}  "
 		puts "     |        |     "
+		puts " "
+		puts " "
 	end
 
 
@@ -28,21 +45,28 @@ class Game
 	def play
 		while @turn <= 9
 			puts "Choose the number of the cell you want to mark"
-			choice = gets.to_i
-	 		if  @turn % 2 == 0
-	 			@board[choice] = 'x'
+			choice = gets.to_i #need to find another method because this never raises an exception. 
 
-	 			$winning_combinations.each do |item| 
-					item.map! { |e| e == choice ? 'x' :  e  }
-				end
+			if choice.class == Fixnum
+		 		if  @turn % 2 == 0
+		 			@board[choice] = 'x'
 
-	 		elsif @turn % 2 != 0
-	 			@board[choice] = 'o'
+		 			$winning_combinations.each do |item| 
+						item.map! { |e| e == choice ? 'x' :  e  }
+					end
 
-	 			$winning_combinations.each do |item| 
-					item.map! { |e| e == choice ? 'o' :  e  }	
-				end
-	 		end
+		 		elsif @turn % 2 != 0
+		 			@board[choice] = 'o'
+
+		 			$winning_combinations.each do |item| 
+						item.map! { |e| e == choice ? 'o' :  e  }	
+					end
+		 		end
+
+	 		elsif choice.class != Fixnum
+				puts "That is not a valid number"
+			end
+
 	 		print draw_board 
 	 		@turn += 1
 	 		check_for_winners
@@ -54,10 +78,10 @@ class Game
  		$winning_combinations.each do |combo|
  			if combo == ['x', 'x', 'x'] 
  				puts 'x wins!!!'
- 				new_game
+ 				greet
  			elsif combo == ['o', 'o', 'o']
  				puts 'o wins!!!'
- 				new_game
+ 				greet
  			end	
  		end
  	end
@@ -66,13 +90,13 @@ class Game
 		if @turn > 9 
 			puts "Game Over"
 			puts " "
-			new_game
-		else
+			greet
+		elsif @turn <= 9
 			play
 		end
 	end
 
 end
 
-Game.new.new_game
+Game.new.greet
 
---
+++ spec/new_specs.rb
@@ -0,0 +1,13 @@
+class Test
+	puts "hi".class
+	puts true.class
+	puts 3.class
+
+	x = gets.chomp
+
+	puts x.class
+
+end
+
+Test.new
+
